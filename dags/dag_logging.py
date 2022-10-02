de32355204562c2b6f4e7adfb927906cb5fbe55a
haozhenhua <1305510482@qq.com>
2022-10-02 21:23:29 +0900

    update

--- dags/dag_logging.py
+++ dags/dag_logging.py
@@ -1,34 +1,51 @@
 import pendulum
-import google.cloud.logging
-import logging
+from google.cloud import logging
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 # 实例化一个日志客户端
-logging_client = google.cloud.logging.Client()
+logging_client = logging.Client()
 # 要写入的日志的名称
-log_name = "mylog"
+log_name = "dag_logging"
 # 选择要写入的日志
 logger = logging_client.logger(log_name)
-logger.log_text('DAG 开始')
-logging.warning('DAG 开始 python logging')
-print('DAG 开始 python print')
 
-my_dag = DAG(
-    'with_dag_logging',
-    description='使用TeskFlow测试Cloud logging 客户端.',
+dag = DAG(
+    'dag_logging',
+    description='使用 dag变量的写法 测试 Cloud logging 客户端.',
     schedule_interval='0 22 * * *',
     start_date=pendulum.datetime(2022, 1, 1, tz="Asia/Tokyo"),
     catchup=False,
     tags=['demo']
 )
 
-def extract():
-    logger.log_text('DAG 开始')
-    print('调用 API')
-    logger.log_text('DAG 结束')
+def task_start():
+    logger.log_text('dag_logging' + ' DAG 开始')
 
-extract_task = PythonOperator(
-    task_id='task001',
-    python_callable=extract
+def task_001():
+    logger.log_text('dag_logging' + ' TASK001 开始')
+    print('调用 API')
+    logger.log_text('dag_logging' + ' TASK001 结束')
+
+def task_end():
+    logger.log_text('dag_logging' + ' DAG 结束')
+
+task_start = PythonOperator(
+    task_id='task_start',
+    python_callable=task_start,
+    dag=dag
 )
+
+task_001 = PythonOperator(
+    task_id='task_001',
+    python_callable=task_001,
+    dag=dag
+)
+
+task_end = PythonOperator(
+    task_id='task_end',
+    python_callable=task_end,
+    dag=dag
+)
+
+task_start >> task_001 >> task_end
--- dags/taskflow_logging.py
+++ dags/taskflow_logging.py
@@ -1,5 +1,3 @@
-
-import json
 import pendulum
 
 from airflow.decorators import dag, task
@@ -8,30 +6,34 @@ from google.cloud import logging
 # 实例化一个日志客户端
 logging_client = logging.Client()
 # 要写入的日志的名称
-log_name = "taskflow-logging"
+log_name = "taskflow_logging"
 # 选择要写入的日志
 logger = logging_client.logger(log_name)
-logger.log_text('DAG 开始')
+
 @dag(
-    description='使用TeskFlow测试Cloud logging 客户端.',
+    description='使用 TeskFlow 测试Cloud logging 客户端.',
     schedule_interval='0 22 * * *',
     start_date=pendulum.datetime(2022, 1, 1, tz="Asia/Tokyo"),
     catchup=False,
     tags=['demo'],
 )
 def taskflow_logging():
+
+    @task
+    def task_start():
+        logger.log_text('taskflow-logging' + 'DAG 开始')
+
     @task()
-    def task001():
-        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+    def task_001():
+        logger.log_text('taskflow-logging' + ' TASK001 开始')
+        print('API调用开始')
+        logger.log_text('taskflow-logging' + ' TASK001 结束')
 
-        order_data_dict = json.loads(data_string)
-        return order_data_dict
+    @task
+    def task_end():
+        logger.log_text('taskflow-logging' + ' DAG 结束')
 
-    logger.log_text('task 开始')
-    task001()
-    logger.log_text('task 结束')
+    task_start() >> task_001() >> task_end()
 
-logger.log_text('DAG 开始')
 # Airflow 只会加载出现在 DAG 文件顶层的 DAG。 这意味着您不能只使用 @dag 声明一个函数 - 您还必须在 DAG 文件中至少调用一次并将其分配给顶级对象，如下所示：
 dag = taskflow_logging()
-logger.log_text('DAG 结束')
