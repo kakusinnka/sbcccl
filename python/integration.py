b1ad2cdc576514350c311aaace0b1576413c0b67
haozhenhua <1305510482@qq.com>
2022-10-04 00:07:01 +0900

    update

---
+++ python/integration.py
@@ -0,0 +1,17 @@
+import logging
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+
+# 实例化一个日志客户端
+client = google.cloud.logging.Client()
+# 获得 Cloud Logging API 调用的handler, 并提供名称。
+handler = CloudLoggingHandler(client, name="mycustomlog")
+# 返回具有指定名称的logging。
+cloud_logger = logging.getLogger()
+# Set the logging level of this logger.
+cloud_logger.setLevel(logging.INFO)
+
+# Add the specified handler to this logger.
+cloud_logger.addHandler(handler)
+logging.error('bad news003')
+cloud_logger.error('bad news003')
